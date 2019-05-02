e33b18d06bdc33a995f9b01fc2a828de359cb9bb
Spl3en <spl3en.contact@gmail.com>
2019-05-02 16:03:50 +0200

    Add P-Rep TestNet Node

--- public/app/routes.py
+++ public/app/routes.py
@@ -12,5 +12,6 @@ def index():
     servers = []
     servers.append (view.status (model.status ("http://iconation.team:9100", "https://ctz.solidwallet.io"), "ICONation Mainnet Citizen Node"))
     servers.append (view.status (model.status ("http://iconation.team:9000", "https://test-ctz.solidwallet.io"), "ICONation Testnet Citizen Node"))
+    servers.append (view.status (model.status ("http://35.181.101.42:9000", "https://preptest.net.solidwallet.io"), "ICONation Testnet P-Rep Node"))
 
     return render_template ('index.html', constants=constants, servers=servers)
